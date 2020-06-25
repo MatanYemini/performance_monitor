1e18e3d6407268f3725bffbf1bb8c5f011ea6989
Yemini <yeminimatan@gmail.com>
2020-06-25 12:42:41 +0300

    Adding server files to a folder

--- src/socket-main.ts
+++
@@ -1,8 +0,0 @@
-import socketio from 'socket.io';
-
-type SocketServer = socketio.Server;
-type Socket = socketio.Socket;
-
-export default function socketMain(io: SocketServer, socket: Socket | null) {
-  console.log('Someone called me im socket main');
-}
--- src/utils/general/AppSettings.ts
+++
@@ -1,4 +0,0 @@
-export interface IConfig {
-  host: string;
-  port: number;
-}
